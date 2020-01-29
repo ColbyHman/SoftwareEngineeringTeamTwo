148db41e9a9dbae374b3b3c2964eba0697c0d44a
colby <hillmanc@moravian.edu>
2020-01-29 14:16:05 -0500

    Test

---
+++ Colby.java
@@ -0,0 +1,7 @@
+public class Colby {
+
+    public static void main(String[] args) {
+        System.out.println("Colby");
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+
+    Colby colby = new Colby();
+
+}
